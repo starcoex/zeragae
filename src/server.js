6a660accf85a8371361497a2a0a789e156f9938d
starcoex <69073390+starcoex@users.noreply.github.com>
2022-10-25 17:36:12 +0900

    first middleWare setting

--- src/server.js
+++ src/server.js
@@ -3,8 +3,15 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+const gossipMiddleWare = (req, res, next) => {
+  console.log(`going to url : ${req.url}`);
+  next();
+};
+
+const handleHome = (req, res) => {
+  res.send({ message: "Hello" });
+};
+
+app.get("/", gossipMiddleWare, handleHome);
 
 app.listen(PORT, () => console.log(`Zeragae Server http://localhost:${PORT}`));
