4a665ac3bc88ae4cae3401dc37eab3df778f34d9
starcoex <69073390+starcoex@users.noreply.github.com>
2022-10-25 18:33:02 +0900

    Morgan, middleware setting

--- src/server.js
+++ src/server.js
@@ -1,33 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
+
 const app = express();
-
-const gossipMiddleware = (req, res, next) => {
-  console.log(`going to ${req.method} url : ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("Not Allowed");
-  } else {
-    console.log("Allowed, you may continue.");
-    next();
-  }
-};
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   return res.send({ message: "Hello" });
 };
-const handleProtected = (req, res) => {
-  res.send("Protected");
-};
 
-app.use(gossipMiddleware);
-app.use(privateMiddleware);
-app.get("/", gossipMiddleware, handleHome);
-app.get("/protected", handleProtected);
+app.use(logger);
+app.get("/", handleHome);
 
 app.listen(PORT, () => console.log(`Zeragae Server http://localhost:${PORT}`));
