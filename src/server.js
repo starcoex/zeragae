efe0231d3b326c39a2bb825ccd51a1b76f11a318
starcoex <69073390+starcoex@users.noreply.github.com>
2022-10-25 17:49:37 +0900

    two, middleware setting

--- src/server.js
+++ src/server.js
@@ -3,15 +3,31 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const gossipMiddleWare = (req, res, next) => {
-  console.log(`going to url : ${req.url}`);
+const gossipMiddleware = (req, res, next) => {
+  console.log(`going to ${req.method} url : ${req.url}`);
   next();
 };
 
-const handleHome = (req, res) => {
-  res.send({ message: "Hello" });
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("Not Allowed");
+  } else {
+    console.log("Allowed, you may continue.");
+    next();
+  }
 };
 
-app.get("/", gossipMiddleWare, handleHome);
+const handleHome = (req, res) => {
+  return res.send({ message: "Hello" });
+};
+const handleProtected = (req, res) => {
+  res.send("Protected");
+};
+
+app.use(gossipMiddleware);
+app.use(privateMiddleware);
+app.get("/", gossipMiddleware, handleHome);
+app.get("/protected", handleProtected);
 
 app.listen(PORT, () => console.log(`Zeragae Server http://localhost:${PORT}`));
