b683a0dad882694a425fa72bae83426d6d7c326c
starcoex <69073390+starcoex@users.noreply.github.com>
2022-10-27 13:13:24 +0900

    get, post first

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,14 @@ export const see = (req, res) => {
   const video = videos[id - 1];
   res.render("see", { pageTitle: `Watching ${video.title}`, video });
 };
-
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  res.send("Post");
+};
 export const upload = (req, res) => res.send("Video Upload");
-export const edit = (req, res) => res.send("Video Edit");
+
 export const remove = (req, res) => res.send("Video Remove");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { see, upload, edit, remove } from "../controllers/videoController";
+import {
+  see,
+  upload,
+  remove,
+  getEdit,
+  postEdit,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 videoRouter.get("/:id(\\d+)/delete", remove);
 
 export default videoRouter;
