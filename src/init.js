7e73cbf05e6640543f045a7c7fd05a94cb0dcf85
starcoex <69073390+starcoex@users.noreply.github.com>
2022-10-30 14:18:12 +0900

    mongo, session two, secrets

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import app from "./server";
 import "./db";
 import Video from "./models/Video";
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+// require("dotenv").config();
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
@@ -17,10 +18,10 @@ app.set("views", process.cwd() + "/src/views");
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: "hello",
-    resave: true,
-    saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/Zeragae" }),
+    secret: process.env.COOKIE_SERCURET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
