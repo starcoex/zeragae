95c322961960594d3aa2efd89195386a09ff5dd4
starcoex <69073390+starcoex@users.noreply.github.com>
2022-10-30 15:36:57 +0900

    user, github login first

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,3 +2,31 @@ export const see = (req, res) => res.send("Users");
 export const logout = (req, res) => res.send("Logout");
 export const edit = (req, res) => res.send("Edit");
 export const remove = (req, res) => res.send("Remove");
+export const startGithubLogin = (req, res) => {
+  const baseUrl = `https://github.com/login/oauth/authorize`;
+  const config = {
+    client_id: process.env.GITHUB_CLIENT,
+    allow_singup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  res.redirect(finalUrl);
+};
+export const callbackGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GITHUB_CLIENT,
+    cliient_secret: process.env.GITHUB_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const data = await fetch(finalUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const json = await data.json();
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { see, logout, edit, remove } from "../controllers/userController";
+import {
+  see,
+  logout,
+  edit,
+  remove,
+  startGithubLogin,
+  callbackGithubLogin,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/callback", callbackGithubLogin);
 userRouter.get("/remove", remove);
 userRouter.get("/:id(\\d+)", see);
 
