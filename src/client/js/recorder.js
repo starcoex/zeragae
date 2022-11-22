8243cb885df093af0237db790923b8f951eefad0
starcoex <69073390+starcoex@users.noreply.github.com>
2022-11-22 15:11:09 +0900

    Video Recoder finish

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -5,32 +5,32 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {};
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
 
 const handleStop = () => {
   startBtn.innerText = "DownLoad Recording";
   startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleDownload);
   recorder.stop();
 };
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  recorder = new MediaRecorder(stream);
-
-  recorder.start();
-
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.ondataavailable = (event) => {
     videoFile = URL.createObjectURL(event.data);
-    console.log(event.data);
-    console.log(video.srcObject);
-    console.log(videoFile);
     video.srcObject = null;
     video.src = videoFile;
-    video.loop = true;
     video.play();
   };
+  recorder.start();
 };
 
 const init = async () => {
