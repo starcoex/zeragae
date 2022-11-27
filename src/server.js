c9d6726e631ba588a13161800a6b627a7e8b7485
starcoex <69073390+starcoex@users.noreply.github.com>
2022-11-27 13:21:16 +0900

    Video Transcode part1 Setup

--- src/server.js
+++ src/server.js
@@ -12,11 +12,17 @@ import apiRouter from "./routers/apiRouter";
 
 const app = express();
 const logger = morgan("dev");
+
 app.use(logger);
-app.use("/assets", express.static("assets"));
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Embedder-Policy", "credentialless");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
@@ -33,6 +39,7 @@ app.get("/add-on", (req, res, next) => {
 });
 
 app.use(localMiddlewares);
+app.use("/convert", express.static("node_modules/@ffmpeg/core/dist"));
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
 
